f5b5dc8133b7aa2ff7f8e5929ab7f9ee56e5be64
PixelPolo <paulr237@gmail.com>
2026-01-11 08:51:15 +0100

    Add ApplicationConfig skeleton in the related folder according to the hexagonal architecture

---
+++ src/main/java/com/pixelpolo/hexagon/infrastructure/config/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package com.pixelpolo.hexagon.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    // Configuration beans can be defined here if needed in the future
+
+}
